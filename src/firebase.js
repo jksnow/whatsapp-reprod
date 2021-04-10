de54a400fc58ec21610a81648aeee2f8b4f4cba7
Jeff Snow <jeff.snow@finalsite.com>
2021-04-10 16:07:57 -0400

    basics of sidebar, starting chat on right

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGoCBIQnhsidTnz0eqYDDyAgmn-JsudpA",
+    authDomain: "whatsapp-reprod.firebaseapp.com",
+    projectId: "whatsapp-reprod",
+    storageBucket: "whatsapp-reprod.appspot.com",
+    messagingSenderId: "752322679999",
+    appId: "1:752322679999:web:a64004a6bb7953cb51220a",
+    measurementId: "G-P5TBDN2JL1"
+  };
